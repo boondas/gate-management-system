d0e2c87c4110f1e3dc5e19d8af50c6b92ef2ae9f
lesetja <lesetja.samuel@gmail.com>
2024-12-02 08:27:34 +0200

    Fix database connection and update requirements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def connect_to_database():
     # Fetch the DATABASE_URL environment variable
-    DATABASE_URL = os.getenv(" postgres://uf8dggiba24i76:p9c124a134a00bf372ab46ca587a95af4e21473d2dbea5657c9a2b228abbe5bce@c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/de18ue68rrh2g1")
+    DATABASE_URL = os.getenv(postgres://uf8dggiba24i76:p9c124a134a00bf372ab46ca587a95af4e21473d2dbea5657c9a2b228abbe5bce@c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/de18ue68rrh2g1)
     if not DATABASE_URL:
         raise ValueError("DATABASE_URL is not set.")
     
