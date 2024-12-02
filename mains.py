f919970168a8ed7570c8071ab7545791f3259e4f
lesetja <lesetja.samuel@gmail.com>
2024-12-02 07:50:20 +0200

    postgres://uf8dggiba24i76:p9c124a134a00bf372ab46ca587a95af4e21473d2dbea5657c9a2b228abbe5bce@c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/de18ue68rrh2g1

--- mains.py
+++ mains.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 import psycopg2
-
+import os
 app = Flask(__name__)
 
 # Database Connection
+
+
 def connect_to_database():
-    return psycopg2.connect(
-        host="localhost",
-        database="gate_management",
-        user="postgres",
-        password="MAN@13diesel"
-    )
+    DATABASE_URL = os.environ.get( postgres://uf8dggiba24i76:p9c124a134a00bf372ab46ca587a95af4e21473d2dbea5657c9a2b228abbe5bce@c3nv2ev86aje4j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/de18ue68rrh2g1)  # Use the Heroku-provided database URL
+    return psycopg2.connect(DATABASE_URL)
+
 
 # Home Route
 @app.route('/')
